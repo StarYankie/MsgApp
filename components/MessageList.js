e4f3551df63140e5ba817d74a50ffe62fefdb8d3
zsd6th <junghancho@gmail.com>
2021-05-02 15:42:14 +0900

    MsgApp front-end

---
+++ components/MessageList.js
@@ -0,0 +1,22 @@
+import MessageListItem from './MessageListItem.js';
+import lifecycleLogger from '../mixins/lifecycle-logger.mixin.js';
+export default {
+    name: 'MessageList',
+    template: `<ul><message-list-item v-for="item in items"
+    :item="item" :key="item.id" @delete="deleteMessage(item)">
+    </message-list-item></ul>`,
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        deleteMessage(message) {
+            this.$emit('delete', message);
+        }
+    },
+    components: {
+        MessageListItem
+    }
+};
