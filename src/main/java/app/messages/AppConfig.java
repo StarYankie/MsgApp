2bd0c872c50e6cb8b07d9e015a86df0ac29bec44
zsd6th <junghancho@gmail.com>
2021-05-02 22:07:14 +0900

    MsgApp Backend (Hibernate)

--- src/main/java/app/messages/AppConfig.java
+++ src/main/java/app/messages/AppConfig.java
@@ -2,15 +2,33 @@ package app.messages;
 
 import java.beans.BeanProperty;
 import java.util.Arrays;
+import app.messages.AuditingFilter;
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @Configuration
 @ComponentScan("app.messages")
 public class AppConfig {
+    private DataSource dataSource;
+
+    public AppConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setPackagesToScan("app.messages");
+        return sessionFactoryBean;
+    }
+
     @Bean
     public FilterRegistrationBean<AuditingFilter> auditingFilterRegistrationBean() {
         FilterRegistrationBean<AuditingFilter> registration = new FilterRegistrationBean<>();
